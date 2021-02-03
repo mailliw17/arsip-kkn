d098bda8c7b7473a5356a73e77169dceb732635c
mailliw17 <williamwirahardi@gmail.com>
2021-02-03 15:36:54 +0700

    change base url 1

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/arsip-kkn';
+$config['base_url'] = 'https://arsip-kkn.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
